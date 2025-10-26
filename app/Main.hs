5f0316e4c666fd7d8b164d80b143de3b7247544d
Daniel Díaz <diaz_carrete@yahoo.com>
2025-10-26 10:50:11 +0100

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,39 @@
 module Main where
 
+import Data.String
+import Data.Text (Text)
+import Data.Text qualified
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn $ 
+    interpolateStuff
+    [
+        Left "aa",
+        Right Foo,
+        Left "bb"
+    ]
+
+class Interpolate a where
+    interpolate :: a -> String
+
+interpolateStuff :: 
+    (Interpolate a, Monoid s, IsString s) =>
+    [Either String a] ->
+    s
+interpolateStuff = 
+    foldr 
+    (\e rest ->
+        case e of
+            Left s -> fromString s
+            Right a -> fromString (interpolate a) 
+        <> rest
+    ) 
+    (fromString "")
+
+data Foo = Foo 
+
+instance Interpolate Foo where
+    interpolate _ = "this is foo"
+
+fooToText :: Foo -> Text
+fooToText _ = Data.Text.pack "this is foo"
